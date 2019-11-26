5bdfa9bd68e31f0f0483af8269b462d8fe17015b
Kamila Reczkowska <k.reczkowska@gmail.com>
2019-11-26 15:23:18 +0100

    GetApi draft, refactor required

--- js/App.js
+++ js/App.js
@@ -24,110 +24,111 @@ class CurrencyConverter extends Component {
         } );
     }
     
-    setEuro = eur =>{
-        this.setState({
+    setEuro = eur => {
+        this.setState( {
             eur: eur
-        })
+        } )
     }
     
     setPln = pln => {
-        this.setState({
+        this.setState( {
             pln: pln
-        })
+        } )
     }
+    
+    handleSubmit = e => {
+        e.preventDefault();
+    };
+    /* adding the list */
+    addTransaction = ( transaction ) => {
         
-        handleSubmit = e => {
-            e.preventDefault();
-        };
-        /* adding the list */
-        addTransaction = ( transaction ) => {
-            
-            let newTransactionList = [ ...this.state.transactionList, transaction ];
-            this.setState( {
-                transactionList: newTransactionList,
-            } );
-        }
-        handleTransaction = ( e ) => {
-            e.preventDefault();
-            if ( typeof this.addTransaction === 'function' ) {
-                console.log( this.state.transactionName )
-                const obj = { //objekt ktory dodawawny jest do state. to jest lista transakcji. kolejny element listy transakcji
-                    name: this.state.transactionName,
-                    euro: this.state.euro,
-                    pln: this.state.euro * 4.30
-                }
-                this.addTransaction( obj );
+        let newTransactionList = [ ...this.state.transactionList, transaction ];
+        this.setState( {
+            transactionList: newTransactionList,
+        } );
+    }
+    handleTransaction = ( e ) => {
+        e.preventDefault();
+        if ( typeof this.addTransaction === 'function' ) {
+            console.log( this.state.transactionName )
+            const obj = { //objekt ktory dodawawny jest do state. to jest lista transakcji. kolejny element listy transakcji
+                name: this.state.transactionName,
+                euro: this.state.euro,
+                pln: this.state.euro * 4.30
             }
-            
+            this.addTransaction( obj );
         }
-        render()
-        {
-            return (
-                /* fetch test section */
-                <>
-                    <section>
-                        <GetApi setPln={this.setPln} setEuro={this.setEuro}/>
-                    </section>
-                    /* currency form */
-                    <section>
-                        <form onSubmit={this.handleSubmit}>
-                            <div>EURO RATE: 4.30 PLN</div>
-                            <h1>ADD TRANSACTION</h1>
-                            <label>Transaction name
-                                <input type='text' name='transactionName' value={this.state.transactionName}
-                                       onChange={this.handleChange} placeholder='Transaction name'>
-                                </input>
-                            </label>
-                            <label>EUR
-                                <input type='text' name='euro' value={this.state.euro} onChange={this.handleChange}
-                                       placeholder='Numbers only'>
-                                </input>
-                            </label>
-                            <button type='submit' name='calculate' onClick={this.handleTransaction}> Calculate</button>
-                            <label>PLN
-                                <input type='text' name='pln' value={this.state.pln} onChange={this.handleChange}
-                                       readOnly>
-                                </input>
-                            </label>
-                        </form>
-                    </section>
-                    {/* list of all transactions */}
-                    <section>
-                        <div>
-                            <ul>
-                                {this.state.transactionList.map( ( element, index ) => <li key={index}>Transaction
-                                                                                                     PLN: {element.pln}</li> )}
-                            </ul>
-                        </div>
-                    </section>
-                    {/*sum of all transactions*/}
-                    <section>
-                        <div>
-                            <ul>
-                                <h3>Total sum of all transactions</h3>
-                                <li>Amount in Euro: <span></span>
-                                    Amount in PLN:<span></span>
-                                </li>
-                            </ul>
-                        </div>
-                    </section>
-                    {/*biggest transaction*/}
-                    <section>
-                        <div>
-                            <ul>
-                                <h3>The biggest transaction</h3>
-                                <li>Amount in Euro: <span></span>
-                                    Amount in PLN:<span></span>
-                                </li>
-                            </ul>
-                        </div>
-                    </section>
-                </>
-            )
-        }
+        
+    }
+    
+    render() {
+        return (
+            /* fetch test section */
+            <>
+                <section>
+                
+                </section>
+                /* currency form */
+                <section>
+                    <form onSubmit={this.handleSubmit}>
+                        <div>EURO RATE: 4.30 PLN</div>
+                        <h1>NEW TRANSACTION</h1>
+                        <label>Transaction name
+                            <input type='text' name='transactionName' value={this.state.transactionName}
+                                   onChange={this.handleChange} placeholder='Transaction name'>
+                            </input>
+                        </label>
+                        <label>
+                            <GetApi setPln={this.setPln} setEuro={this.setEuro}/>
+                            <input type='text' name='euro' value={this.state.euro} onChange={this.handleChange}
+                                   placeholder='Numbers only'>
+                            </input>
+                        </label>
+                        <button type='submit' name='calculate' onClick={this.handleTransaction}> Calculate</button>
+                        <label>
+                            <GetApi setPln={this.setPln} setEuro={this.setEuro}/>
+                            <input type='text' name='pln' value={this.state.pln} onChange={this.handleChange}
+                                   readOnly>
+                            </input>
+                        </label>
+                    </form>
+                </section>
+                {/* list of all transactions */}
+                <section>
+                    <div>
+                        <ul>
+                            {this.state.transactionList.map( ( element, index ) => <li key={index}>Transaction
+                                                                                                   PLN: {element.pln}</li> )}
+                        </ul>
+                    </div>
+                </section>
+                {/*sum of all transactions*/}
+                <section>
+                    <div>
+                        <ul>
+                            <h3>Total sum of all transactions</h3>
+                            <li>Amount in Euro: <span></span>
+                                Amount in PLN:<span></span>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                {/*biggest transaction*/}
+                <section>
+                    <div>
+                        <ul>
+                            <h3>The biggest transaction</h3>
+                            <li>Amount in Euro: <span></span>
+                                Amount in PLN:<span></span>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </>
+        )
+    }
     
 }
-
 
 
 class App extends Component {
@@ -137,4 +138,5 @@ class App extends Component {
         </>
     }
 }
+
 ReactDOM.render( <App/>, document.getElementById( "app" ) );
--- js/GetApi.js
+++ js/GetApi.js
@@ -1,37 +1,57 @@
 import React, {Component} from "react";
 
-class GetApi extends Component{
-    constructor(props) {
-        super(props);
+
+class GetApi extends Component {
+    constructor( props ) {
+        super( props );
         this.state = {
             currencies: [],
             eur: 0,
-            pln: 0
+            pln: 0,
+            fromValue: '',
+            fromCurrency: '',
+            toValue: '',
+            toCurrency:''
         }
     }
     
     componentDidMount() {
-        fetch("https://api.exchangerate-api.com/v4/latest/EUR" ).then(r => r.json())
-            .then(data => {
-                this.setState({pln: data.rates.EUR});
-                this.props.setEuro(data.rates.EUR);
-            })
-        fetch("https://api.exchangerate-api.com/v4/latest/PLN").then(r => r.json())
-            .then(data => {
-                this.setState({eur: data.rates.PLN});
-                this.props.setPln(data.rates.PLN);
-            })
-      }
+        fetch( "https://api.exchangerate-api.com/v4/latest/EUR" ).then( r => r.json() )
+            .then( data => {
+                this.setState( {eur: data.rates.EUR} );
+                this.props.setEuro( data.rates.EUR );
+            } )
+        fetch( "https://api.exchangerate-api.com/v4/latest/PLN" ).then( r => r.json() )
+            .then( data => {
+                this.setState( {pln: data.rates.PLN} );
+                this.props.setPln( data.rates.PLN );
+            } )
     
+        // fetch( "https://api.exchangerate-api.com/v4/latest/EUR" ).then( r => r.json() )
+        //     .then( data => {
+        //         this.setState( {eur: data.rates.EUR} );
+        //         this.props.setEuro( data.rates.EUR );
+        //     } )
+        // fetch( "https://api.exchangerate-api.com/v4/latest/PLN" ).then( r => r.json() )
+        //     .then( data => {
+        //         this.setState( {pln: data.rates.PLN} );
+        //         this.props.setPln( data.rates.PLN );
+        //     } )
+    }
     
     
     render() {
-       
+        console.log(this.state.pln);
         return <>
-        <div>xxxx{this.state.eur}</div>
-            <div>{this.state.pln}</div>
+            <select>
+                <option value=''>EUR
+                </option>
+                <option value=''>PLN
+                </option>
+            </select>
         </>
     }
 }
+
 export default GetApi;
 
