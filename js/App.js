32a5f0d643d3b480126a664fc868e78b7ef9954c
Kamila Reczkowska <k.reczkowska@gmail.com>
2019-11-27 20:02:16 +0100

    sass components done

--- js/App.js
+++ js/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
-import './../style/app.scss';
+import '../style/main.scss';
 import CurrencyValue from "./CurrencyValue";
 import CurrencyRates from './components/CurrencyRates';
 
@@ -83,94 +83,96 @@ class CurrencyConverter extends Component {
     }
     
     calculateToValue = () => {
-      if (this.state.fromValue ==='' || this.state.toCurrency ==='' || !this.state.rates[this.state.toCurrency]){
+        if ( this.state.fromValue === '' || this.state.toCurrency === '' || !this.state.rates[this.state.toCurrency] ) {
             return 0;
         }
-       
-       // this.setState({
-       //     change state, assign to the field w liscie
-       // })
+        
+        // this.setState({
+        //     change state, assign to the field w liscie
+        // })
         return this.state.fromValue * this.state.rates[this.state.toCurrency];
     }
     
     render() {
         return (
             <>
-                {/*currency form */}
-                <section className={'main_app'}>
-                    <div>
-                        <h1 className={'container'}>CURRENCY CONVERTER</h1>
-                        <form onSubmit={this.handleSubmit} className={'container'}>
-                            <div className={'exchange'}>
-                                <div>
-                                    <label>Transaction name
-                                        <input type='text' name='transactionName' value={this.state.transactionName}
-                                               onChange={this.handleChange} placeholder='Transaction name'
-                                               className={'input_fields'}>
+                <div className={'mainView'}>
+                    {/*currency form */}
+                    <section className={'main_app'}>
+                        <div>
+                            <h1 className={'container'}>CURRENCY CONVERTER</h1>
+                            <form onSubmit={this.handleSubmit} className={'container'}>
+                                <div className={'exchange'}>
+                                    <div>
+                                        <label>Transaction name
+                                            <input type='text' name='transactionName' value={this.state.transactionName}
+                                                   onChange={this.handleChange} placeholder='Transaction name'
+                                                   className={'input_fields'}>
+                                            </input>
+                                        </label>
+                                    </div>
+                                    
+                                    <div>
+                                        <label>From:
+                                            <CurrencyValue setRates={this.setRates}/>
+                                            <input type='text' name='fromValue' value={this.state.fromValue}
+                                                   onChange={this.handleChange}
+                                                   placeholder='Numbers only' className={'input_fields'}>
+                                            </input>
+                                        </label>
+                                    </div>
+                                    
+                                    <div>
+                                        <CurrencyRates setToCurrency={this.setToCurrency}/>
+                                        <input type='text' name='toValue' value={this.calculateToValue()}
+                                               placeholder='Numbers only' className={'input_fields'} readOnly>
                                         </input>
-                                    </label>
+                                    </div>
+                                    
+                                    <button type='submit' name='calculate' onClick={this.handleTransaction}
+                                            className={'btn'}> Calculate
+                                    </button>
                                 </div>
-                                
-                                <div>
-                                    <label>From:
-                                        <CurrencyValue setRates={this.setRates}/>
-                                        <input type='text' name='fromValue' value={this.state.fromValue}
-                                               onChange={this.handleChange}
-                                               placeholder='Numbers only' className={'input_fields'}>
-                                        </input>
-                                    </label>
-                                </div>
-                                
-                                <div>
-                                    <CurrencyRates setToCurrency={this.setToCurrency}/>
-                                    <input type='text' name='toValue' value={this.calculateToValue()}
-                                           placeholder='Numbers only' className={'input_fields'} readOnly>
-                                    </input>
-                                </div>
-                                
-                                <button type='submit' name='calculate' onClick={this.handleTransaction}
-                                        className={'btn'}> Calculate
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </section>
-                {/* list of all transactions */}
-                <section className={'main_app'}>
-                    <div className={'container'}>
-                        <ul className={'transaction_list'}>
-                            <li>THIS IS A TRANSACTION LIST</li>
-                            {this.state.transactionList.map( ( element, index ) => <li key={index}>
-                                <span> Transaction Name:<span> {this.state.transactionName}</span></span>
-                                <span> Transaction EUR:<span>{this.state.fromValue}</span></span>
-                                <span> Transaction PLN: <span>{this.state.toValue}</span></span>
-                            </li> )}
-                        </ul>
-                    </div>
-                </section>
-                {/*sum of all transactions*/}
-                <section className={'main_app'}>
-                    <div className={'container '}>
-                        <ul className={'transaction_list transactions'} onClick={this.handleChange}>
-                            <h3>Total sum of all transactions</h3>
-                            <li>
-                                <span>Amount in Euro: <span>{this.state.eur}</span> </span>
-                                <span> Amount in PLN:<span>{this.state.pln}</span></span>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
-                {/*biggest transaction*/}
-                <section className={'main_app'}>
-                    <div className={'container'}>
-                        <ul className={'transaction_list transactions'}>
-                            <h3>The biggest transaction</h3>
-                            <li>Amount in Euro: <span>'100'</span>
-                                Amount in PLN:<span>'100'</span>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
+                            </form>
+                        </div>
+                    </section>
+                    {/* list of all transactions */}
+                    <section className={'main_app'}>
+                        <div className={'container'}>
+                            <ul className={'transaction_list'}>
+                                <li>THIS IS A TRANSACTION LIST</li>
+                                {this.state.transactionList.map( ( element, index ) => <li key={index}>
+                                    <span> Transaction Name:<span> {this.state.transactionName}</span></span>
+                                    <span> Transaction EUR:<span>{this.state.fromValue}</span></span>
+                                    <span> Transaction PLN: <span>{this.state.toValue}</span></span>
+                                </li> )}
+                            </ul>
+                        </div>
+                    </section>
+                    {/*sum of all transactions*/}
+                    <section className={'main_app'}>
+                        <div className={'container '}>
+                            <ul className={'transaction_list transactions'} onClick={this.handleChange}>
+                                <h3>Total sum of all transactions</h3>
+                                <li>
+                                    <span>Amount in Euro: <span>{this.state.eur}</span> </span>
+                                    <span> Amount in PLN:<span>{this.state.pln}</span></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </section>
+                    {/*biggest transaction*/}
+                    <section className={'main_app'}>
+                        <div className={'container'}>
+                            <ul className={'transaction_list transactions'}>
+                                <h3>The biggest transaction</h3>
+                                <li>Amount in Euro: <span>'100'</span>
+                                    Amount in PLN:<span>'100'</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
             </>
         )
     }
